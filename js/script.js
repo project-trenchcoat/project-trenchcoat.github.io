c8c9b0b865bf4f2151bd5cd307b0aba7345de00a
Christie Xu <christie@CX-OMEN.localdomain>
2019-11-27 17:53:43 -0800

    initial commit basic structure

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){  
+    $("#button1").click(function(e) {
+      e.preventDefault();
+      $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+      }, 1200);
+    });
+  });
