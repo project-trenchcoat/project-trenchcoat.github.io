d60de5ff990333ead5fde8aa953aa678be58bed2
meepitschree <xuchrist@usc.edu>
2020-03-31 20:47:22 -0500

    reworked site template

--- js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){  
-    $("#button1").click(function(e) {
-      e.preventDefault();
-      $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-      }, 1000);
-    });
-  });
